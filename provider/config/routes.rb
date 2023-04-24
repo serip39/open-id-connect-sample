eaefad25eb44bd7b93da8f404d4374c8b07d2a11
serip39 <seri@fusic.co.jp>
2023-04-24 16:40:51 +0900

    install gem doorkeeper, doorkeeper-openid_connect

--- provider/config/routes.rb
+++ provider/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  use_doorkeeper_openid_connect
+  use_doorkeeper
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
